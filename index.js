1190cfd9dafcfa6d7819dfe24721a46fc59af916
OtkurO <>
2021-09-08 22:34:57 +0200

    add routes and path parameters

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const path = require('path');
 const port = 3000;
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
-app.get('/cats', (req, res) => res.send(path.join(__dirname, '/cats.html')));
-app.get('/dogs', (req, res) => {
-  // res.send(path.join(__dirname, '/dogs.html'));
-  res.send({ dog1: 'Paw', dog2: 'Hund' });
+app.get('/cats', (req, res) => {
+  res.send('<h1>Cats Pages</h1>');
 });
+app.get('/cats/black', (req, res) => {
+  res.send('<h1>Cats Pages--- black cats</h1>');
+});
+app.post('/cats/black', (req, res) => {
+  res.send('<h1>black cats response</h1>');
+  console.log('Log from /cats/black');
+});
+app.get('/dogs', (req, res) => res.send('<h1>Dogs Pages</h1>'));
+app.get('/cats/:subject', (req, res) =>
+  res.send(`<h1>Generic /cats/ ${req.params.subject}</h1>`)
+);
 
 app.use(() => {
   console.log('We got a new request!!!!');
