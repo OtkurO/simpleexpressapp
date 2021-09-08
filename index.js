2721fa2b90a9ac94a8f7d7d3016cf6a530172b31
OtkurO <>
2021-09-08 10:06:29 +0200

    add nondemon

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const path = require('path');
 const port = 3000;
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
+app.get('/cats', (req, res) => res.send(path.join(__dirname, '/cats.html')));
+app.get('/dogs', (req, res) => {
+  // res.send(path.join(__dirname, '/dogs.html'));
+  res.send({ dog1: 'Paw', dog2: 'Hund' });
+});
 
 app.use(() => {
   console.log('We got a new request!!!!');
