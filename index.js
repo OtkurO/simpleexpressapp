02b1048aa2ddf8d0a612a665e47fab49513e982a
OtkurO <>
2021-09-13 09:24:18 +0200

    set views path and render home page with ejs

--- index.js
+++ index.js
@@ -3,28 +3,34 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.set('view engin', 'ejs');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
-app.get('/cats', (req, res) => {
-  res.send('<h1>Cats Pages</h1>');
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
+
+app.get('/', (req, res) => {
+  res.render('home');
 });
-app.get('/cats/black', (req, res) => {
-  res.send('<h1>Cats Pages--- black cats</h1>');
-});
-app.post('/cats/black', (req, res) => {
-  res.send('<h1>black cats response</h1>');
-  console.log('Log from /cats/black');
-});
-app.get('/dogs', (req, res) => res.send('<h1>Dogs Pages</h1>'));
-app.get('/cats/search/', (req, res) => {
-  let response = '<h1>Cats Page Search:</h1>';
-  for (const key in req.query) {
-    response += `<h2>${key}:  ${req.query[key]};`;
-  }
-  res.send(response);
-  console.log(req.query);
-});
+
+// app.get('/cats/black', (req, res) => {
+//   res.send('<h1>Cats Pages--- black cats</h1>');
+// });
+// app.post('/cats/black', (req, res) => {
+//   res.send('<h1>black cats response</h1>');
+//   console.log('Log from /cats/black');
+// });
+
+// // using query string parameters in route (example:/search/?q=cat&id=12)
+// app.get('/cats/search/', (req, res) => {
+//   let response = '<h1>Cats Page Search:</h1>';
+//   for (const key in req.query) {
+//     response += `<h2>${key}:  ${req.query[key]};`;
+//   }
+//   res.send(response);
+//   console.log(req.query);
+// });
+
+//using req path parameters (:parameter)
 // app.get('/cats/q=:subject&:id', (req, res) =>
 //   res.send(
 //     `<h1>cats/ Subject:${req.params.subject}, ID: ${req.params.id} </h1>`
@@ -32,11 +38,7 @@ app.get('/cats/search/', (req, res) => {
 // );
 
 app.get('*', (req, res) => {
-  res.send('<h1>Page Not found</h1>');
-});
-
-app.use(() => {
-  console.log('We got a new request!!!!');
+  res.send('<h1> 404 --Page Not found</h1>');
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}!`));
